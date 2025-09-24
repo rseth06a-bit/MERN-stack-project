da3c667d274d04a408ea23914aadc567dcc434d9
rseth06a <rseth06a@gmail.com>
2025-09-23 17:31:17 -0700

    env update

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://rseth06a_db_user:2fat-5cJymC.YmW@mern-stack-project.bzg57jz.mongodb.net/books-collection?retryWrites=true&w=majority&appName=MERN-stack-project'
+export const mongoDBURL = process.env.MONGODB_URL;
